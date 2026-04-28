0f11a882e2aa73862f13829cb47993cc6c598dda
Nischal Joshi <nischalzoc999@gmail.com>
2026-04-28 18:47:46 +0545

    install setup

--- Backend/index.js
+++ Backend/index.js
@@ -1,37 +1,36 @@
+require("dotenv").config();
 //Imported File for database
-require("./database/connection")
+require("./database/connection");
 
 // Imported files for saveData and getData for excell file
-const saveData = require('./routes/saveDataRoute')
-const getData = require('./routes/getDataRoute')
+const saveData = require("./routes/saveDataRoute");
+const getData = require("./routes/getDataRoute");
 
 // Imported Map routes for wards and districts
-const getWards = require('./routes/getWardRoute')
-const getDistricts = require('./routes/getDistrictRoute')
-
+const getWards = require("./routes/getWardRoute");
+const getDistricts = require("./routes/getDistrictRoute");
 
 // Library imports
-const express = require('express')
-const cors = require('cors')
-const status = require('express-status-monitor')
-require('dotenv').config({ quiet: true })
+const express = require("express");
+const cors = require("cors");
+const status = require("express-status-monitor");
+require("dotenv").config({ quiet: true });
 
 //Object Initilization
-const app = express()
+const app = express();
 
 //Server modifications
-app.use(cors())
-app.use(express.json())
-app.use(status())
+app.use(cors());
+app.use(express.json());
+app.use(status());
 
 //Routes
-app.use('/data', saveData, getData)
+app.use("/data", saveData, getData);
 
 //Map Routes
-app.use('/map', getWards, getDistricts)
-
+app.use("/map", getWards, getDistricts);
 
 //Server Running
 app.listen(process.env.PORT, () => {
-    console.log(`Server running on port ${process.env.PORT}`)
-})
+  console.log(`Server running on port ${process.env.PORT}`);
+});
